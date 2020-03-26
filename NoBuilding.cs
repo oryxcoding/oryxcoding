50236a7997dd9ae9f4e628d5adcba3bbbd754a55
oryxcoding <51522724+oryxcoding@users.noreply.github.com>
2020-03-26 22:09:36 +0400

    NoBuilding
    Only with permission can build

---
+++ NoBuilding.cs
@@ -0,0 +1,32 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("No Building", "Oryx", "1.0.0")]
+    [Description("Only with permission can build")]
+
+    public class NoBuilding : RustPlugin
+    {
+        public readonly string perm = "nobuilding.allow";
+
+        private void Init()
+        {
+            permission.RegisterPermission(perm, this);
+        }
+
+        object CanBuild(Planner planner, Construction prefab, Construction.Target target)
+        {
+            BasePlayer player = planner.GetOwnerConnection().player as BasePlayer;
+
+            if(player == null)
+            {
+                return null;
+            }
+
+            if (permission.UserHasPermission(player.UserIDString, perm))
+            {
+                return null;
+            }
+
+            return false;
+        }
+    }
+}
